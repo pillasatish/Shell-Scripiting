5bdd09c16fff2ba33c6c9d3af4995d1180f334cc
SATISH <pillasatish7@gmail.com>
2023-01-14 23:00:53 +0000

    shell scripting

--- sample.sh
+++ sample.sh
@@ -4,3 +4,13 @@ echo "Hai"
 
 # print the colour code
 echo -e "\e[45;33m i am yellow \e[0m"
+
+#Variable is something which holds the values dynamically.
+
+a=10
+b=abc
+# no concept of data type in bash scripting
+# so here is 10 is a string
+ 
+ echo $a #$a is printing the value of a
+ echo $b
