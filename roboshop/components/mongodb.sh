06d3bf1d23094e655cfac005662c8fc928bba9fe
SATISH <pillasatish7@gmail.com>
2023-01-15 22:48:51 +0000

    shell scripting

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -4,7 +4,7 @@ set -e #exit the code if any one of the below code is not executed
 COMPONENT=mongoDB
 LOGFILE="/tmp/$COMPONENT.log"
 MONGODB_REPO_URL="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
-
+COMPONENT_REPO_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 
 source components/common.sh
 
@@ -22,17 +22,32 @@ stat $?
 
 #Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
 
-echo "Updating the configuration file mongod.conf"
+echo -n "Updating the configuration file mongod.conf"
 #Config file:  /etc/mongod.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
-#systemctl restart mongod
+echo -n "restart the $COMPONENT"
+systemctl restart mongod &>> $LOGFILE
+stat $?
 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+echo -n "Downloading the $COMPONENT schema"
 
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
+curl -s -L -o /tmp/mongodb.zip $COMPONENT_REPO_URL
+stat $?
+
+cd /tmp
+echo -n "Extract the schema"
+unzip mongodb.zip &>> $LOGFILE
+stat $?
+
+
+cd mongodb-main
+echo -n "Injecting the schema"
+mongo < catalogue.js  
+mongo < users.js &>> $LOGFILE
+stat $?
+
+echo -n "________________________$COMPONENT configuration is completed_________________________"
+
+
