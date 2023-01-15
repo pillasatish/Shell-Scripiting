b0c21da15e94130bd02bd991e93a9f69fbd498f2
SATISH <pillasatish7@gmail.com>
2023-01-15 22:12:50 +0000

    shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,22 +7,40 @@ LOGFILE="/tmp/$COMPONENT.log"
 
 source components/common.sh
 
-echo -n "Installing Nginx"
-yum install nginx -y &>> $LOGFILE
-if [ $? -eq 0 ] ; then
+stat() {
+    if [ $? -eq 0 ] ; then
     echo -e "\e[32m SUCCESS\e[0m"
 else
     echo -e "\e[32m FAILURE\e[0m"
 
 fi
-# systemctl enable nginx
-# systemctl start nginx
+}
 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -n "Installing Nginx"
+yum install nginx -y &>> $LOGFILE
+stat $?
+
+systemctl enable nginx &>> $LOGFILE
+
+echo -n "Starting nginx"
+systemctl start nginx
+stat $?
+
+echo -n "Downloading $COMPONENT: "
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
+
+echo -n "Clearing the old component"
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+echo -n "Extracting the $COMPONENT"
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
