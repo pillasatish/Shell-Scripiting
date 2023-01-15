56e8e4a24e85ce28a09729b677401f5ce489c02a
SATISH <pillasatish7@gmail.com>
2023-01-15 23:48:50 +0000

    shell scripting

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -37,7 +37,7 @@ echo -n "extrating the $COMPONENT: "
 cd /home/roboshop
 unzip /tmp/${COMPONENT}.zip &>> $LOGFILE
 mv ${COMPONENT}-main ${COMPONENT} && chown -R $APPUSER:$APPUSER $COMPONENT
-cd /home/roboshop/${COMPONENT}
+cd ${COMPONENT}
 stat $?
 
 
