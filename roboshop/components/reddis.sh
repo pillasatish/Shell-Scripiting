fd5dadceb4fdd75e1bd6d14aef4b863ef8c0949d
SATISH <pillasatish7@gmail.com>
2023-01-16 00:42:14 +0000

    shell scripting

--- roboshop/components/reddis.sh
+++ roboshop/components/reddis.sh
@@ -9,17 +9,17 @@ APPUSER="roboshop"
 source components/common.sh
 
 echo -n "Configuring the $COMPONENT repo: "
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>$LOGFILE
 stat $?
 
 echo -n "Installing the redis: "
-yum install redis-6.2.7 -y
+yum install redis-6.2.7 -y &>> $LOGFILE
 stat $?
 
 #Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
 echo -n "update the bind ip address: "
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf 
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf
 stat $?
 
