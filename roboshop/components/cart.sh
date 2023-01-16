87644ec0a0c70b77b23523cd2550b893070a7816
SATISH <pillasatish7@gmail.com>
2023-01-16 01:24:53 +0000

    shell scripting

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#!/bin/bash
+
 set -e #exit the code if any one of the below code is not executed
+COMPONENT=cart
+LOGFILE="/tmp/$COMPONENT.log"
+APPUSER="roboshop"
+
 
 source components/common.sh
+
+#calling nodejs function form common.sh file
+NODEJS
