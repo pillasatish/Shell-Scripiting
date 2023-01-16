7d3af857ca0e5c7592ba7c8b9ddc4347f08d275c
SATISH <pillasatish7@gmail.com>
2023-01-16 23:13:00 +0000

    shell scripting

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -23,11 +23,15 @@ systemctl enable mysqld
 systemctl start mysqld &>> $LOGFILE
 stat $?
 
-echo "changing the default root password: "
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpassword_change.sql
-DEFAULT_ROOT_PASSWORD=$(sudo grep "temporary password"  /var/log/mysqld.log | awk '{print $NF}')
-mysql --connect-expired-password -uroot -p"$DEFAULT_ROOT_PASSWORD" < /tmp/rootpassword_change.sql
-stat $?
+#only run for the first time
+echo "show databases" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
+if [ 0 -ne $? ]; then
+    echo "changing the default root password: "
+    echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpassword_change.sql
+    DEFAULT_ROOT_PASSWORD=$(sudo grep "temporary password"  /var/log/mysqld.log | awk '{print $NF}')
+    mysql --connect-expired-password -uroot -p"$DEFAULT_ROOT_PASSWORD" < /tmp/rootpassword_change.sql
+    stat $?
+fi
 
 
 
