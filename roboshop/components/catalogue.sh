f39cc4329c37cb46a0c34d80674737d7a501799b
SATISH <pillasatish7@gmail.com>
2023-01-16 00:27:55 +0000

    shell scripting

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -8,13 +8,8 @@ APPUSER="roboshop"
 
 source components/common.sh
 
-echo -n "Downlaoding nodejs repo"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash &>> $LOGFILE
-stat $?
-
-echo -n "Insatlling the nodejs: "
-yum install nodejs -y &>> $LOGFILE
-stat $?
+#calling nodejs function form common.sh file
+NODEJS
 
 #Let's now set up the catalogue application.
 
@@ -22,44 +17,8 @@ stat $?
 
 #So to run the catalogue service we choose to run as a normal user and that user name should be more relevant to the project. Hence we will use `roboshop` as the username to run the service.
 
-echo -n "Adding the common Useradd: "
- id roboshop &>> $LOGFILE || useradd roboshop 
- stat $?
-
-echo -n "swtich to roboshop and install the $COMPONENT: "
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
-
-echo -n "cleanup:"
-cd /home/roboshop/ && rm -rf ${COMPONENT} &>> $LOGFILE
-stat $?
-
-echo -n "extrating the $COMPONENT: "
-cd /home/roboshop
-unzip /tmp/${COMPONENT}.zip &>> $LOGFILE
-mv ${COMPONENT}-main ${COMPONENT} && chown -R $APPUSER:$APPUSER $COMPONENT
-cd ${COMPONENT}
-stat $?
 
 
-
-
-echo -n "Insatlling the $COMPONENT: "
-npm install &>> $LOGFILE
-stat $?
-
-#Update SystemD file with correct IP addresses
-#Update `MONGO_DNSNAME` with MongoDB Server IP
-echo -n "systemD file: "
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop-internal/' systemd.service
-stat $?
-
-echo -n "setup the service with systemctl: "
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload &>> $LOGFILE
-systemctl start catalogue &>> $LOGFILE
-systemctl enable catalogue &>> $LOGFILE
-stat $?
 
 
 
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -15,3 +15,68 @@ else
 
 fi
 }
+
+NODEJS() {
+    echo -n "Downlaoding nodejs repo"
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | bash &>> $LOGFILE
+    stat $?
+
+    echo -n "Insatlling the nodejs: "
+    yum install nodejs -y &>> $LOGFILE
+    stat $?
+
+    #calling user creation function
+    CREATE_USER
+
+    # calling function downlaod and eactract
+    DOWNLAOD_AND_EXTRACT
+    #Installing the nodejs
+    echo -n "Insatlling the $COMPONENT: "
+    npm install &>> $LOGFILE
+    stat $?
+
+    # calling config service
+    CONFIG_SERVICE
+
+    # calling the start service
+    START_SERVICE
+}
+
+CREATE_USER() {
+    echo -n "Adding the common Useradd: "
+    id roboshop &>> $LOGFILE || useradd roboshop 
+    stat $?
+}
+
+DOWNLAOD_AND_EXTRACT() {
+    echo -n "swtich to roboshop and install the $COMPONENT: "
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $?
+
+    echo -n "cleanup:"
+    cd /home/roboshop/ && rm -rf ${COMPONENT} &>> $LOGFILE
+    stat $?
+
+    echo -n "extrating the $COMPONENT: "
+    cd /home/roboshop
+    unzip /tmp/${COMPONENT}.zip &>> $LOGFILE
+    mv ${COMPONENT}-main ${COMPONENT} && chown -R $APPUSER:$APPUSER $COMPONENT
+    cd ${COMPONENT}
+    stat $?
+}
+
+CONFIG_SERVICE(){
+    echo -n "systemD file: "
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop-internal/' systemd.service
+    stat $?
+}
+
+START_SERVICE() {
+    echo -n "setup the service with systemctl: "
+    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/catalogue.service
+    systemctl daemon-reload &>> $LOGFILE
+    systemctl start $COMPONENT &>> $LOGFILE
+    systemctl enable $COMPONENT &>> $LOGFILE
+    stat $? 
+
+}
