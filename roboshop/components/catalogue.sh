d0fb3899676bc582ac05863198ee47ca0000bb4d
SATISH <pillasatish7@gmail.com>
2023-01-16 00:06:20 +0000

    shell scripting

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -55,7 +55,7 @@ sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop-internal/' systemd.service
 stat $?
 
 echo -n "setup the service with systemctl: "
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload &>> $LOGFILE
 systemctl start catalogue &>> $LOGFILE
 systemctl enable catalogue &>> $LOGFILE
